2e606189d016dfbbbe3efeb35466954fc5bc536b
tomiock <ockier1@gmail.com>
2025-05-14 13:16:38 +0200

    feat: inference well done

--- model/embedding_model.py
+++ model/embedding_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 from pytorch_metric_learning.losses import TripletMarginLoss
-from pytorch_metric_learning.miners import TripletMarginMiner
+from pytorch_metric_learning.miners import TripletMarginMiner, BaseMiner
 from pytorch_metric_learning.distances import LpDistance
 
 from torch.utils.data import Dataset, DataLoader
@@ -11,7 +11,19 @@ from torch import nn
 
 from tqdm import tqdm
 
-from typing import Optional
+from typing import Optional, Callable
+
+
+def create_features(features, data):
+    return_data = []
+    for col in features:
+        col_data = data[col]
+        if isinstance(col_data, (list, np.ndarray)):
+            return_data.extend(col_data)
+        elif isinstance(col_data, (int, float, np.number)):
+            return_data.extend([col_data])
+
+    return return_data
 
 
 class PositivesDataset(Dataset):
@@ -19,11 +31,9 @@ class PositivesDataset(Dataset):
         self.dataframe = dataframe
 
         self.pair_labels = self.dataframe.index.tolist()
-        self.samples = []
 
-        for i in range(len(dataframe)):
-            self.samples.append((i, self.pair_labels[i], 0))
-            self.samples.append((i, self.pair_labels[i], 1))
+        self.task_samples = []
+        self.translator_samples = []
 
         self.task_features = [
             "SOURCE_LANG_task",
@@ -45,141 +55,122 @@ class PositivesDataset(Dataset):
             "translator_categorical_vector",
         ]
 
+        for i in range(len(dataframe)):
+            row_data = self.dataframe.iloc[i]
+
+            data_task = create_features(self.task_features, row_data)
+            data_translator = create_features(self.translator_features, row_data)
+
+            features_task = np.array(data_task, dtype=np.float32)
+            features_translator = np.array(data_translator, dtype=np.float32)
+
+            self.task_samples.append(features_task)
+            self.translator_samples.append(features_translator)
+
+        assert len(self.translator_samples) == len(self.task_samples)
+
     def __len__(self):
         return len(self.dataframe)
 
     def __getitem__(self, idx):
-        original_row_idx, label, entity_type = self.samples[idx]
-        row_data = self.dataframe.iloc[original_row_idx]
+        task = self.task_samples[idx]
+        translator = self.translator_samples[idx]
+        pair_label = self.pair_labels[idx]
 
-        if entity_type == 0:
-            feature_list = []
-            for col in self.task_features:
-                col_data = row_data[col]
-                if isinstance(col_data, (list, np.ndarray)):
-                    feature_list.extend(col_data)
-                elif isinstance(col_data, (int, float, np.number)):
-                    feature_list.append(col_data)
-
-            features = np.array(feature_list, dtype=np.float32)
-
-        elif entity_type == 1:
-            feature_list = []
-            for col in self.translator_features:
-                col_data = row_data[col]
-                if isinstance(col_data, (list, np.ndarray)):
-                    feature_list.extend(col_data)
-                elif isinstance(col_data, (int, float, np.number)):
-                    feature_list.append(col_data)
-            features = np.array(feature_list, dtype=np.float32)
-
-        else:
-            raise ValueError("entity_type must be 0 or 1")
-
-        features_tensor = torch.from_numpy(features)
-        label_tensor = torch.tensor(label, dtype=torch.float32)
-        entity_type_tensor = torch.tensor(entity_type, dtype=torch.float32)
-
-        return features_tensor, label_tensor, entity_type_tensor
+        return torch.tensor(task), torch.tensor(translator), torch.tensor(pair_label)
 
 
-def collate_fn(batch):
-    return tuple(zip(*batch))
-
-
-class Autoencoder(nn.Module):
+class Translator_AE(nn.Module):
     def __init__(
         self,
-        task_dim: int,
         translator_dim: int,
         latent_dim: int,
         hidden_dim: int = 64,
     ):
-        super(Autoencoder, self).__init__()
+        super(Translator_AE, self).__init__()
 
-        self.task_dim = task_dim
         self.translator_dim = translator_dim
         self.latent_dim = latent_dim
         self.hidden_dim = hidden_dim
 
-        self.encoder_task = nn.Sequential(
-            nn.Linear(task_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, latent_dim),
-        )
-
-        self.encoder_translator = nn.Sequential(
+        self.encoder = nn.Sequential(
             nn.Linear(translator_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, latent_dim),
         )
 
-        self.decoder_task = nn.Sequential(
-            nn.Linear(latent_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, task_dim),
-        )
-
-        self.decoder_translator = nn.Sequential(
+        self.decoder = nn.Sequential(
             nn.Linear(latent_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, translator_dim),
         )
 
-    def forward(self, batch, entity):
-        if entity == 0:
-            task_embeddings = self.encoder_task(batch)
-            task_recontrucctions = self.decoder_task(task_embeddings)
-            return task_embeddings, task_recontrucctions
-
-        if entity == 1:
-            translator_embeddings = self.encoder_translator(batch)
-            translator_recontrucctions = self.decoder_translator(translator_embeddings)
-            return translator_embeddings, translator_recontrucctions
+    def forward(self, batch):
+        translator_embeddings = self.encoder(batch)
+        translator_recontrucctions = self.decoder(translator_embeddings)
+        return translator_embeddings, translator_recontrucctions
 
 
-def train_step(train_dataloader, loss_func, miner_func):
+class Task_AE(nn.Module):
+    def __init__(
+        self,
+        task_dim: int,
+        latent_dim: int,
+        hidden_dim: int = 64,
+    ):
+        super(Task_AE, self).__init__()
+
+        self.task_dim = task_dim
+        self.latent_dim = latent_dim
+        self.hidden_dim = hidden_dim
+
+        self.encoder = nn.Sequential(
+            nn.Linear(task_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, latent_dim),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, task_dim),
+        )
+
+    def forward(self, batch):
+        task_embeddings = self.encoder(batch)
+        task_recontrucctions = self.decoder(task_embeddings)
+        return task_embeddings, task_recontrucctions
+
+
+def train_step(
+    train_dataloader: torch.utils.data.DataLoader,
+    loss_func,
+    miner_func: BaseMiner,
+    op_func_task: torch.optim.Optimizer,
+    op_func_trans: torch.optim.Optimizer,
+):
     num_batches = 0
     total_epoch_metric_loss = 0
     avg_epoch_metric_loss = None
-    for features, labels, entity_types in train_dataloader:
-        all_embeddings = []
-        all_labels = []
-        all_reconstruccions = []
 
-        for i in range(len(features)):
-            single_features = features[i]
-            single_label = labels[i]
-            single_entity = entity_types[i]
+    # all of these are in batches
+    for translators, tasks, pair_labels in train_dataloader:
+        optimizer_translator.zero_grad()
+        optimizer_task.zero_grad()
 
-            if single_entity.item() == 0:  # this is a task
-                embeddings, reconstruction = model(single_features, single_entity)
+        trans_embed, trans_output = model_translator.train()(translators)
+        task_embed, task_output = model_task.train()(tasks)
 
-            elif single_entity.item() == 1:  # this is a tranlator
-                embeddings, reconstruction = model(single_features, single_entity)
+        concat_embed = torch.cat([task_embed, trans_embed], dim=0)
+        concat_labels = torch.cat([pair_labels, pair_labels], dim=0)
 
-            else:
-                raise ValueError
+        hard_triplets = miner_func(concat_embed, concat_labels)
 
-            all_embeddings.append(embeddings)
-            all_labels.append(single_label)
-            all_reconstruccions.append(reconstruction)
-
-        all_embeddings = torch.stack(all_embeddings)
-        all_labels = torch.stack(all_labels)
-        all_reconstruccions = torch.stack(all_reconstruccions)
-
-        hard_triplets = miner_func(all_embeddings, all_labels)
-
-        metric_loss_value = torch.tensor(0.0)
-
-        if len(hard_triplets) > 0:
-            metric_loss_value = loss_func(all_embeddings, all_labels, hard_triplets)
-        else:
-            print("ERRORRR")
-
+        metric_loss_value = loss_func(concat_embed, concat_labels, hard_triplets)
         metric_loss_value.backward()
-        optimizer.step()
+
+        op_func_task.step()
+        op_func_trans.step()
 
         total_epoch_metric_loss += metric_loss_value.item()
         num_batches += 1
@@ -191,37 +182,19 @@ def train_step(train_dataloader, loss_func, miner_func):
     return avg_epoch_metric_loss
 
 
-def eval_step(dataloader, loss_func):
+def eval_step(dataloader: torch.utils.data.DataLoader, loss_func: Callable):
     num_batches = 0
     total_epoch_metric_loss = 0
     avg_epoch_metric_loss = None
-    for features, labels, entity_types in dataloader:
-        all_embeddings = []
-        all_labels = []
-        all_reconstruccions = []
 
-        for i in range(len(features)):
-            single_features = features[i]
-            single_label = labels[i]
-            single_entity = entity_types[i]
+    for translators, tasks, pair_labels in dataloader:
+        trans_embed, trans_output = model_translator.eval()(translators)
+        task_embed, task_output = model_task.eval()(tasks)
 
-            if single_entity.item() == 0:  # this is a task
-                embeddings, reconstruction = model(single_features, single_entity)
+        concat_embed = torch.cat([task_embed, trans_embed], dim=0)
+        concat_labels = torch.cat([pair_labels, pair_labels], dim=0)
 
-            elif single_entity.item() == 1:  # this is a tranlator
-                embeddings, reconstruction = model(single_features, single_entity)
-
-            else:
-                raise ValueError
-
-            all_embeddings.append(embeddings)
-            all_labels.append(single_label)
-            all_reconstruccions.append(reconstruction)
-
-        all_embeddings = torch.stack(all_embeddings)
-        all_labels = torch.stack(all_labels)
-
-        metric_loss_value = loss_func(all_embeddings, all_labels)
+        metric_loss_value = loss_func(concat_embed, concat_labels)
 
         total_epoch_metric_loss += metric_loss_value.item()
         num_batches += 1
@@ -233,34 +206,23 @@ def eval_step(dataloader, loss_func):
     return avg_epoch_metric_loss
 
 
-def inference(model: nn.Module, batch: tuple, device: Optional[torch.device] = None):
-    model.eval()
+def inference(
+    model_tasks: nn.Module,
+    model_translators: nn.Module,
+    batch: tuple,
+    device: Optional[torch.device] = None,
+):
+    assert len(batch) == 3
 
     if device is None:
         device = torch.device("cpu")  # defaulting to cpu
 
-    features_tuple, labels_tuple, entity_tuples = batch
+    translators, tasks, _ = batch
 
-    embeddings_info = []
-    with torch.no_grad():
-        for i in range(len(features_tuple)):
-            single_features = features_tuple.to(device)
-            single_label = labels_tuple.item()
-            single_entity = entity_tuples.item()
+    trans_embed, _ = model_translators.eval()(translators)
+    task_embed, _ = model_tasks.eval()(tasks)
 
-            print(single_features.shape)
-
-            if single_entity == 0:
-                embedding = model.encoder_task(single_features.unsqueeze(0))
-            elif single_entity == 1:
-                embedding = model.decoder_task(single_features.unsqueeze(0))
-            else:
-                raise ValueError("invalid entity type")
-
-            single_embedding = embedding.squeeze(0)
-            embeddings_info.append((single_embedding, single_label, single_entity))
-
-    return embeddings_info
+    return trans_embed, task_embed
 
 
 if __name__ == "__main__":
@@ -269,15 +231,26 @@ if __name__ == "__main__":
     dataset = PositivesDataset(positives)
     train_set, val_set = torch.utils.data.random_split(dataset, [0.8, 0.2])
 
+    BATCH_SIZE = 128
+
     train_dataloder = DataLoader(
-        train_set, batch_size=32, shuffle=True, collate_fn=collate_fn
+        train_set,
+        batch_size=BATCH_SIZE,
+        shuffle=True,  # important
     )
     val_dataloder = DataLoader(
-        val_set, batch_size=32, shuffle=True, collate_fn=collate_fn
+        val_set,
+        batch_size=BATCH_SIZE,
+        shuffle=True,  # important
     )
 
-    model = Autoencoder(
+    model_task = Task_AE(
         task_dim=42,
+        latent_dim=10,
+        hidden_dim=36,
+    )
+
+    model_translator = Translator_AE(
         translator_dim=42,
         latent_dim=10,
         hidden_dim=36,
@@ -286,14 +259,21 @@ if __name__ == "__main__":
     miner = TripletMarginMiner()
     loss = TripletMarginLoss()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    optimizer_translator = torch.optim.Adam(model_translator.parameters(), lr=0.0001)
+    optimizer_task = torch.optim.Adam(model_task.parameters(), lr=0.0001)
 
-    num_epochs = 1
+    num_epochs = 10
 
     for epoch in range(num_epochs):
-        train_loss = train_step(train_dataloder, loss, miner)
+        train_loss = train_step(
+            train_dataloder, loss, miner, optimizer_task, optimizer_translator
+        )
         val_loss = eval_step(val_dataloder, loss)
 
         print(f"loss: {train_loss:.6f}, val_loss: {val_loss:.6f}")
 
     batch_test = val_set[0]
+
+    translators_embedded, tasks_embedded = inference(
+        batch=batch_test, model_tasks=model_task, model_translators=model_translator
+    )
